0d25d4fddd3e039aaff8ce80ac5f35f0afa1e740
NAKAGAKI Kenji <nakaken0629@gmail.com>
2016-04-29 16:24:54 +0900

    アプリのディレクトリーを修正

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -1,5 +1,5 @@
 # paths
-app_path = "/home/example/sidekiq-sample"
+app_path = "/var/www/sidekiq-sample"
 working_directory "#{app_path}/current"
 pid               "#{app_path}/current/tmp/pids/unicorn.pid"
 
