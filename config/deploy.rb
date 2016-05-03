8e6e3cb32ae96873c1eae0ff3459803435dbbdfb
NAKAGAKI Kenji <nakaken0629@gmail.com>
2016-05-03 23:37:29 +0900

    sidekiqの設定を追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,7 @@ set :rbenv_ruby, '2.3.1'
 # set :rbenv_ruby, File.read('.ruby-version').strip
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails sidekiq sidekiqctl}
 set :rbenv_roles, :all # default value
 
 namespace :deploy do
